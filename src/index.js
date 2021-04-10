d380cd78580a0b2a8f998216c61168fd0bff1322
Tomas Ausino Palau <tausinopalau@gmail.com>
2021-04-10 01:03:40 -0300

    debug setTimeout, juego finalizado

--- src/index.js
+++ src/index.js
@@ -2,41 +2,41 @@ let patronUsuario = [];
 let patronMaquina = [];
 let ronda = 0
 
-const $estado = document.querySelector(".estado")
 
 
 
-document.querySelector(".inicio").onclick = comenzarJuego
+
+document.querySelector(".inicio").onclick = comenzarJuego;
 
 function comenzarJuego() {
-
-    // resetearPatrones()
+    resetearPatrones()
     manejarRonda()
 }
-
 function manejarRonda() {
 
     const $nuevoCuadro = obtenerCuadroAleatorio();
-    actualizarEstado("Turno de la máquina");
+    actualizarEstado("Turno de la máquina"); 
     patronMaquina.push($nuevoCuadro);
 
     const RETRASO_USUARIO = ((patronMaquina.length + 1) * 1000);
 
     patronMaquina.forEach(function($cuadro, index) {
         const RETRASO_MS = (index + 1) * 1000;
+        console.log($cuadro)
         setTimeout(function() {
             activarCuadro($cuadro);
         }, RETRASO_MS)
 
     })
 
-    setTimeout = ((function() {
+    setTimeout((function() {
         actualizarEstado("Turno del Jugador")
         desbloquearInputUsuario()
     }), RETRASO_USUARIO)
     
-
-
+    patronUsuario = [];
+    ronda++;
+    actualizarNumeroRonda(ronda);
 
 
 }
@@ -53,7 +53,7 @@ function manejarInputUsuario(e) {
         return;
     } 
 
-    if (patronUsuario.length === secuenciaMaquina.length) {
+    if (patronUsuario.length === patronMaquina.length) {
         bloquearInputUsuario();
         setTimeout(manejarRonda, 1000);
     }
@@ -71,14 +71,24 @@ function obtenerCuadroAleatorio () {
 
 function activarCuadro($cuadro) {
     $cuadro.style.opacity = 1;
-    setTimeout(function() {
-        $cuadro.style.opacity = 0.5;
-    }, 500);
-  }
+    console.log($cuadro)
+    desactivarCuadro($cuadro)
+}
 
+function desactivarCuadro($cuadro) {
+    setTimeout(() => {
+        $cuadro.style.opacity = 0.5;
+    }, 500); 
+}
 
 function actualizarEstado (txt) {
+    const $estado = document.querySelector(".turno")
     $estado.innerText = txt;
+}
+
+function actualizarNumeroRonda(number) {
+    const $ronda = document.querySelector(".numero-ronda")
+    $ronda.innerText = number;
 }
 
 function desbloquearInputUsuario() {
@@ -90,7 +100,6 @@ function desbloquearInputUsuario() {
 function bloquearInputUsuario() {
     document.querySelectorAll(".cuadro").forEach(function ($cuadro) {
         $cuadro.onclick = function () {
-
         };
     })
 }
@@ -99,3 +108,9 @@ function perder() {
     bloquearInputUsuario();
     actualizarEstado('Perdiste! Tocá "Empezar" para jugar de nuevo!');
   }
+
+function resetearPatrones() {
+     patronUsuario = [];
+     patronMaquina = [];
+     ronda = 0
+}
